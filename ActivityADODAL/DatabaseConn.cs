b7f3558f6594140bd498840811341660ee74ec38
subhamltts <subham.panda@ltts.com>
2021-08-04 22:46:16 +0530

    Revisit

---
+++ ActivityADODAL/DatabaseConn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityADODAL
+{
+    public class DatabaseConn
+    {
+        public static SqlConnection sqlConn = null;
+        public static SqlConnection ConnectToDB()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["ProjectXConStr"].ConnectionString;
+            try
+            {
+                return new SqlConnection(connStr);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
